0a88f77e5447ad96c586e8134e9c27aaca5f4f2b
Prahlad Ram <prahlad.ram@traveloka.com>
2019-10-29 16:28:26 +0530

    Tagging Service Endpoint

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 resource "aws_vpc_endpoint_service" "service_provider" {
   acceptance_required        = "${var.acceptance_required}"
   network_load_balancer_arns = ["${var.nlb_arns}"]
+
+  tags = {
+    Name             = "${var.service_name}"
+    Description      = "${var.description}"
+    Environment      = "${var.environment}"
+    ProductDomain    = "${var.product_domain}"
+    ManagedBy        = "terraform"
+  }
 }
 
 resource "aws_vpc_endpoint_service_allowed_principal" "service_provider" {
   vpc_endpoint_service_id = "${aws_vpc_endpoint_service.service_provider.id}"
   count                   = "${length(var.allowed_principals)}"
   principal_arn           = "${element(var.allowed_principals, count.index)}"
+
 }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,24 @@ variable "acceptance_required" {
   default     = false
   description = "Whether or not VPC endpoint connection requests to the service must be accepted by the service owner"
 }
+
+variable "service_name" {
+  type        = "string"
+  description = "Stack name of the Service Endpoint"
+}
+
+variable "description" {
+  type        = "string"
+  default     = ""
+  description = "Description of the Service Endpoint"
+}
+
+variable "environment" {
+  type        = "string"
+  description = "Will be used in resources' Environment tag"
+}
+
+variable "product_domain" {
+  type        = "string"
+  description = "Abbreviation of the product domain the created resources belong to"
+}
