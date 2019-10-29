39dac0d952819ddce355f0fcf901739084990ce8
Febry Antonius <febry.antonius@traveloka.com>
2019-10-29 16:14:02 +0700

    Add default tagging and fix issue force new resource when one of item in list of allowed principals is removed

--- examples/create-endpoint-service/main.tf
+++ examples/create-endpoint-service/main.tf
@@ -6,10 +6,15 @@ provider "aws" {
 # Local values declaration. Change the value with a valid value before you used this example
 #############################################################################################
 locals {
-  nlb_arns            = ["arn:aws:elasticloadbalancing:ap-southeast-1:012345678901:loadbalancer/net/service-provider-nlb/01ab234c5d67e8f9"]
-  allowed_principals  = ["arn:aws:iam::123456789012:root"]
+  nlb_arns = ["arn:aws:elasticloadbalancing:ap-southeast-1:583729182359:loadbalancer/net/service-provider-nlb/01ab234c5d67e8f9"]
+
+  allowed_principals = [
+    "arn:aws:iam::123456789012:role/SuperAdmin",
+    "arn:aws:iam::210987654321:role/SuperAdmin",
+  ]
+
   acceptance_required = false
-  service_name        = abctest
+  service_name        = "abctest"
 }
 
 #######################
@@ -18,7 +23,7 @@ locals {
 module "service-provider" {
   source = "../../"
 
-  nlb_arns = "${local.nlb_arns}"
+  nlb_arns       = "${local.nlb_arns}"
   service_name   = "${local.service_name}"
   product_domain = "abc"
   environment    = "staging"
