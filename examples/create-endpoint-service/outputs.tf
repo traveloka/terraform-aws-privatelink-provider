55d646972a19e6f8b48c966f389085df7fadd65b
Nur Adhi Nugroho <nur.nugroho@traveloka.com>
2020-08-26 12:37:56 +0700

    Support Terraform v0.12

--- examples/create-endpoint-service/outputs.tf
+++ examples/create-endpoint-service/outputs.tf
@@ -1,4 +1,5 @@
 output "service_provider_name" {
   description = "The name of VPC Endpoint Service"
-  value       = "${module.service-provider.service_provider_name}"
+  value       = module.service-provider.service_provider_name
 }
+
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_endpoint_service" "service_provider" {
-  acceptance_required        = "${var.acceptance_required}"
-  network_load_balancer_arns = ["${var.nlb_arns}"]
-  allowed_principals         = ["${var.allowed_principals}"]
+  acceptance_required        = var.acceptance_required
+  network_load_balancer_arns = var.nlb_arns
+  allowed_principals         = var.allowed_principals
 
-  tags = "${merge(var.additional_tags, local.default_tags)}"
+  tags = merge(var.additional_tags, local.default_tags)
 }
